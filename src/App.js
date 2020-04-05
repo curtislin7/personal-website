5a23991e80ce98a9f512d1e69f34b5b1ddcc968f
curtislin7 <curtislin7@gmail.com>
2020-04-05 17:15:54 -0600

    updating readme

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import HomePage from './Pages/Home/HomePage'
-// import ResumePage from './Pages/Resume/ResumePage'
-import Navigation from './components/Nav'
-import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core/styles';
 import { theme } from './AppTheme';
 
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      {/* <Navigation/> */}
       <Switch>
-      <Route exact path="/" component={HomePage}/>
-        {/* <Route path="/resume" component={ResumePage} /> */}
+        <Route exact path="/" component={HomePage}/>
       </Switch>
     </ThemeProvider>
   )
