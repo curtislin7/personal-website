5c553e85f203a5cc894e050d03a9a65927a9c1aa
curtislin7 <curtislin7@gmail.com>
2020-04-05 18:21:23 -0600

    Updates

---
+++ precache-manifest.8f5a32bccbc0f05de5e066b265af9bdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa19ceddbf6b34ddda18e674960a2d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdc6d7cd430ef320d966",
+    "url": "/static/css/main.73de8b9f.chunk.css"
+  },
+  {
+    "revision": "a296b578177861158b05",
+    "url": "/static/js/2.0e44905d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0e44905d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdc6d7cd430ef320d966",
+    "url": "/static/js/main.d6a868ea.chunk.js"
+  },
+  {
+    "revision": "905e4d6bd7d9946d806f",
+    "url": "/static/js/runtime-main.8ab15afb.js"
+  }
+]);
