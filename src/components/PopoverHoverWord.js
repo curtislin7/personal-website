428aa786cb5b9ce4e5d9275eeb4b395cac2b6db8
curtislin7 <curtislin7@gmail.com>
2021-04-03 21:52:54 -0600

    Fix date in job experience, remove unused dependency

--- src/components/PopoverHoverWord.js
+++ src/components/PopoverHoverWord.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Popover from '@material-ui/core/Popover';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
